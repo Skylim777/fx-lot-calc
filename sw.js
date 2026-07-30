de03d7a8a0b0b57d9972f5694b797cad1e717c06
Skylim777 <cobhc.kissing.the.shadows@gmail.com>
2026-07-31 01:06:11 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fx-lot-calc-v1";
+const CACHE = "fx-lot-calc-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,6 +6,7 @@ const ASSETS = [
   "./icon-192.png",
   "./icon-512.png",
   "./icon-maskable-512.png",
+  "./apple-touch-icon.png",
 ];
 
 self.addEventListener("install", (event) => {
